cfbc57174feaf28ce46c10c290e2580b658cefaf
Remco Tolsma <info@remcotolsma.nl>
2015-08-26 13:50:27 +0200

    Added unserializer class.

---
+++ src/XML/Unserializer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Unserializer
+ *
+ * @link       http://pear.php.net/package/XML_Serializer/docs
+ * @since      1.0.0
+ *
+ * @package    Pronamic/WP/Twinfield
+ */
+
+namespace Pronamic\WP\Twinfield\XML;
+
+/**
+ * Unserializer
+ *
+ * @since      1.0.0
+ * @package    Pronamic/WP/Twinfield
+ * @author     Remco Tolsma <info@remcotolsma.nl>
+ */
+abstract class Unserializer {
+	/**
+	 * Constructs and initalizes an unserializer object
+	 */
+	public function __construct() {
+		
+	}
+}
