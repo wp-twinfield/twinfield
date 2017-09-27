288e8e0c09fa6d3bbfae49f02221c1cab91d73ad
Remco Tolsma <info@remcotolsma.nl>
2017-09-27 13:13:20 +0200

    Added a document codes contstants class.

---
+++ src/Declarations/DocumentCodes.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Document codes
+ *
+ * @since      1.0.0
+ *
+ * @package    Pronamic/WP/Twinfield
+ */
+
+namespace Pronamic\WP\Twinfield\Declarations;
+
+/**
+ * Document codes
+ *
+ * This class contains constants for different Twinfield declaration document codes.
+ *
+ * @since      1.0.0
+ * @package    Pronamic/WP/Twinfield
+ * @author     Remco Tolsma <info@remcotolsma.nl>
+ */
+class DocumentCodes {
+    /**
+     * VAT Turnover.
+     *
+     * @var string
+     */
+    const VATTURNOVER = 'VATTURNOVER';
+
+    /**
+     * VAT ICT
+     *
+     * @var string
+     */
+    const VATICT = 'VATICT';
+
+    /**
+     * Tax group.
+     *
+     * @var string
+     */
+    const TAXGROUP = 'TAXGROUP';
+}
