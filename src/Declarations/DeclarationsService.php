e89b6ebe1d96840d11a7cf23c869b5b08cd332f0
Remco Tolsma <info@remcotolsma.nl>
2017-09-27 14:23:46 +0200

    Added support for more document codes.

--- src/Declarations/DeclarationsService.php
+++ src/Declarations/DeclarationsService.php
@@ -38,6 +38,13 @@ class DeclarationsService extends AbstractClient {
 		parent::__construct( self::WSDL_FILE, $session );
 	}
 
+	/**
+	 * Get all sumaries of the specified office code.
+	 *
+	 * @see https://c5.twinfield.de/webservices/documentation/#/ApiReference/Miscellaneous/Declaration
+	 * @param string $office_code The code of the office for which the returns should be retrieved. Mandatory.
+	 * @return array
+	 */
 	public function get_all_summaries( $office_code ) {
 		$parameters = new \stdClass();
 		$parameters->companyCode = $office_code;
@@ -45,29 +52,135 @@ class DeclarationsService extends AbstractClient {
 		return $this->soap_client->GetAllSummaries( $parameters );
 	}
 
-	public function get_payment_reference( $document_id ) {
+	/**
+	 * Get payment reference for the specified document code and ID.
+	 *
+	 * @see https://c5.twinfield.de/webservices/documentation/#/ApiReference/Miscellaneous/Declaration
+	 * @param string $document_id
+	 * @param string $document_code
+	 * @return string
+	 */
+	public function get_payment_reference( $document_id, $document_code = null ) {
+		$function = 'GetVatReturnPaymentReference';
+
+		switch ( $document_code ) {
+			case DocumentCodes::VATTURNOVER:
+				$function = 'GetVatReturnPaymentReference';
+
+				break;
+			case DocumentCodes::TAXGROUP:
+				$function = 'GetTaxGroupVatReturnPaymentReference';
+
+				break;
+		}
+
 		$parameters = new \stdClass();
 		$parameters->documentId = $document_id;
 
-		$response = $this->soap_client->GetVatReturnPaymentReference( $parameters );
+		$response = $this->soap_client->__soapCall( $function, array( $parameters ) );
 
 		if ( isset( $response->paymentReference ) ) {
 			return $response->paymentReference;
 		}
 	}
 
-	public function get_vat_return_as_xbrl( $document_id ) {
+	/**
+	 * Get VAT return payment reference for the specified document ID.
+	 *
+	 * @see https://c5.twinfield.de/webservices/documentation/#/ApiReference/Miscellaneous/Declaration
+	 * @param string $document_id
+	 * @return string
+	 */
+	public function get_vat_return_payment_reference( $document_id ) {
+		return $this->get_payment_reference( $document_id, DocumentCodes::VATTURNOVER );
+
+	}
+
+	/**
+	 * Get TAX group payment reference for the specified document ID.
+	 *
+	 * @see https://c5.twinfield.de/webservices/documentation/#/ApiReference/Miscellaneous/Declaration
+	 * @param string $document_id
+	 * @return string
+	 */
+	public function get_tax_group_vat_return_payment_reference( $document_id ) {
+		return $this->get_payment_reference( $document_id, DocumentCodes::TAXGROUP );
+
+	}
+
+	/**
+	 * Get XBRL for the specified document code and ID.
+	 *
+	 * @see https://c5.twinfield.de/webservices/documentation/#/ApiReference/Miscellaneous/Declaration
+	 * @param string $document_id
+	 * @param string $document_code
+	 * @return string
+	 */
+	public function get_xbrl( $document_id, $document_code = null ) {
+		$function = 'GetVatReturnAsXbrl';
+
+		switch ( $document_code ) {
+			case DocumentCodes::VATTURNOVER:
+				$function = 'GetVatReturnAsXbrl';
+
+				break;
+			case DocumentCodes::VATICT:
+				$function = 'GetIctReturnAsXbrl';
+
+				break;
+			case DocumentCodes::TAXGROUP:
+				$function = 'GetTaxGroupVatReturnAsXbrl';
+
+				break;
+		}
+
 		$parameters = new \stdClass();
 		$parameters->documentId = $document_id;
 		$parameters->isMessageIdRequired = true;
 
-		$response = $this->soap_client->GetVatReturnAsXbrl( $parameters );
+		$response = $this->soap_client->__soapCall( $function, array( $parameters ) );
 
 		if ( isset( $response->vatReturn, $response->vatReturn->any ) ) {
 			return $response->vatReturn->any;
 		}
 	}
 
+	/**
+	 * Get VAT return XBRL for the specified document ID.
+	 *
+	 * @see https://c5.twinfield.de/webservices/documentation/#/ApiReference/Miscellaneous/Declaration
+	 * @param string $document_id
+	 * @return string
+	 */
+	public function get_vat_return_as_xbrl( $document_id ) {
+		return $this->get_xbrl( $document_id, DocumentCodes::VATTURNOVER );
+	}
+
+	/**
+	 * Get ICT VAT return XBRL for the specified document ID.
+	 *
+	 * @see https://c5.twinfield.de/webservices/documentation/#/ApiReference/Miscellaneous/Declaration
+	 * @param string $document_id
+	 * @return string
+	 */
+	public function get_ict_return_as_xbrl( $document_id ) {
+		return $this->get_xbrl( $document_id, DocumentCodes::VATICT );
+	}
+
+	/**
+	 * Get TAX group VAT return XBRL for the specified document ID.
+	 *
+	 * @see https://c5.twinfield.de/webservices/documentation/#/ApiReference/Miscellaneous/Declaration
+	 * @param string $document_id
+	 * @return string
+	 */
+	public function get_tax_group_vat_return_as_xbrl( $document_id ) {
+		return $this->get_xbrl( $document_id, DocumentCodes::TAXGROUP );
+	}
+
+	/**
+	 * Test.
+	 */
 	public function test() {
 		var_dump( $test->vatReturn );
 
