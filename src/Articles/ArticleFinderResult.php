fd0aff54d67a1a29e72ea423acdc62be68e8c3b0
Remco Tolsma <info@remcotolsma.nl>
2018-03-29 16:19:59 +0200

    Implemented the article finder.

---
+++ src/Articles/ArticleFinderResult.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Article finder result
+ *
+ * @since      1.0.0
+ *
+ * @package    Pronamic/WP/Twinfield
+ */
+
+namespace Pronamic\WP\Twinfield\Articles;
+
+use Pronamic\WP\Twinfield\Contacts\Contact;
+
+/**
+ * Article finder result
+ *
+ * This class represents an Twinfield article
+ *
+ * @since      1.0.0
+ * @package    Pronamic/WP/Twinfield
+ * @author     Remco Tolsma <info@remcotolsma.nl>
+ */
+class ArticleFinderResult {
+	/**
+	 * Code.
+	 *
+	 * @var string
+	 */
+	private $code;
+
+	/**
+	 * Name.
+	 *
+	 * @var string
+	 */
+	private $name;
+
+	/**
+	 * Constructs and initializes an customer find result.
+	 */
+	public function __construct() {
+
+	}
+
+	/**
+	 * Get code.
+	 *
+	 * @return string
+	 */
+	public function get_code() {
+		return $this->code;
+	}
+
+	/**
+	 * Set code.
+	 *
+	 * @param string $code The code.
+	 */
+	public function set_code( $code ) {
+		$this->code = $code;
+	}
+
+	/**
+	 * Get name.
+	 *
+	 * @return string
+	 */
+	public function get_name() {
+		return $this->name;
+	}
+
+	/**
+	 * Set name.
+	 *
+	 * @param string $name The name.
+	 */
+	public function set_name( $name ) {
+		$this->name = $name;
+	}
+}
--- src/Articles/ArticlesFinder.php
+++ src/Articles/ArticlesFinder.php
@@ -10,3 +10,79 @@
 
 namespace Pronamic\WP\Twinfield\Articles;
 
+use Pronamic\WP\Twinfield\Finder;
+use Pronamic\WP\Twinfield\FinderTypes;
+use Pronamic\WP\Twinfield\Search;
+use Pronamic\WP\Twinfield\SearchFields;
+
+/**
+ * Articles Finder
+ *
+ * @since      1.0.0
+ * @package    Pronamic/WP/Twinfield
+ * @author     Remco Tolsma <info@remcotolsma.nl>
+ */
+class ArticlesFinder {
+	/**
+	 * The finder wich is used to connect with Twinfield.
+	 *
+	 * @var Finder
+	 */
+	private $finder;
+
+	/**
+	 * Constructs and initializes an custom finder object.
+	 *
+	 * @param Finder $finder The finder.
+	 */
+	public function __construct( Finder $finder ) {
+		$this->finder = $finder;
+	}
+
+	/**
+	 * Find articles.
+	 *
+	 * @param string $pattern   The pattern.
+	 * @param string $field     The field.
+	 * @param int    $first_row The first row.
+	 * @param int    $max_rows  The max rows.
+	 * @param array  $options   The options.
+	 * @return array
+	 */
+	public function get_articles( $pattern, $field, $first_row, $max_rows, $options = array() ) {
+		$articles = array();
+
+		// Request.
+		$search = new Search(
+			FinderTypes::ART,
+			$pattern,
+			$field,
+			$first_row,
+			$max_rows,
+			$options
+		);
+
+		$response = $this->finder->search( $search );
+
+		// Parse.
+		if ( $response ) {
+			if ( $response->is_successful() ) {
+				$data = $response->get_data();
+
+				$items = $data->get_items();
+
+				if ( ! is_null( $items ) ) {
+					foreach ( $items as $item ) {
+						$article = new ArticleFinderResult();
+						$article->set_code( $item[0] );
+						$article->set_name( $item[1] );
+
+						$articles[] = $article;
+					}
+				}
+			}
+		}
+
+		return $articles;
+	}
+}
