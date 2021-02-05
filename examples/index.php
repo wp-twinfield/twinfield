ea7a645934f4bb75f8918420d6be4d4aebd2083e
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2021-02-05 15:20:31 +0100

    wip

--- examples/index.php
+++ examples/index.php
@@ -2,6 +2,23 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$client_id     = '';
+$client_secret = '';
+$redirect_uri  = '';
+$state         = \bin2hex( \openssl_random_pseudo_bytes( 32 ) );
+
+if ( 'POST' === $_SERVER['REQUEST_METHOD'] ) {
+	$client_id     = $_POST['client_id'];
+	$client_secret = $_POST['client_secret'];
+	$redirect_uri  = $_POST['redirect_uri'];
+}
+
+$openid_connect_provider = new \Pronamic\WP\Twinfield\Authentication\OpenIdConnectProvider( $client_id, $client_secret, $redirect_uri );
+
+$openid_connect_provider->maybe_handle_twinfield_return( function( $data ) {
+	var_dump( $data );
+} );
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -18,10 +35,23 @@ require __DIR__ . '/../vendor/autoload.php';
 		</p>
 
 		<form method="post" action="">
-			Client ID <input type="text" name="client_id" />
-			Client Secret <input type="text" name="client_secret" />
-			Redirect URI <input type="url" name="redirect_uri" />
+			Client ID <input type="text" name="client_id" value="<?php echo \htmlspecialchars( $client_id ); ?>" />
+			Client Secret <input type="text" name="client_secret" value="<?php echo \htmlspecialchars( $client_secret ); ?>" />
+			Redirect URI <input type="url" name="redirect_uri" value="<?php echo \htmlspecialchars( $redirect_uri ); ?>" />
 			<button type="submit">Connect</button>
 		</form>
+
+		<p>
+			<?php
+
+			$url = $openid_connect_provider->get_authorize_url( $state );
+
+			printf(
+				'<a href="%s">Connect with Twinfield</a>',
+				\htmlspecialchars( $url )
+			);
+
+			?>
+		</p>
 	</body>
 </html>
